a40f37052d1e7595490bc0885b23bb36ebdcaca6
Rick Rieger <rickrieger36251@gmail.com>
2021-05-23 19:29:33 -0400

    changed stuff

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.json("Welcome to Games info App");
+  res.json({message: "Welcome to Games info App"});
 });
 
 module.exports = router;
